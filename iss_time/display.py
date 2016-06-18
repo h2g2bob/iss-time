5c0c6be66d61b631225bec5cde430961bee66f39
user <user@lorbanery>
2016-06-18 15:36:27 +0100

    more output

--- iss_time/display.py
+++ iss_time/display.py
@@ -2,24 +2,26 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import sqlite3
+import time
 
 from .constants import ISS_TIME_DB
 
 
-def get_most_recent():
+def get_most_recent(limit):
 	conn = sqlite3.connect(ISS_TIME_DB)
 	try:
-		[row] = conn.execute('SELECT nowutc, lat, lng, tzdata, issnow, description, tzabbr, country FROM datapoints ORDER BY nowutc DESC LIMIT 1;')
-		return row
+		rows = conn.execute('SELECT nowutc, lat, lng, tzdata, issnow, description, tzabbr, country FROM datapoints ORDER BY nowutc DESC LIMIT ?;', (limit,))
+		return list(rows)
 	finally:
 		conn.close()
 
 
 def main():
-	_nowutc, _lat, _lng, _tzdata, issnow, _description, _tzabbr, country = get_most_recent()
-	print('Time on space station is: {issnow} ({country})'.format(
-		issnow=issnow,
-		country=country))
+	for nowutc, _lat, _lng, _tzdata, issnow, _description, _tzabbr, country in get_most_recent(120):
+		print('At {nowutc}, time on space station is: {issnow} ({country})'.format(
+			nowutc=time.ctime(nowutc),
+			issnow=issnow,
+			country=country))
 
 if __name__ == '__main__':
 	main()
